6724b249f53f08bc2316daf1b31e9bb7d835cd97
Aluno IFSC <IFSC@CDR-718114.cacador.ifsc.edu.br>
2021-11-16 16:13:23 -0300

    Impreção de dados com JOptionPane

--- src/githubrobson/GithubRobson.java
+++ src/githubrobson/GithubRobson.java
@@ -4,18 +4,28 @@
  * and open the template in the editor.
  */
 package githubrobson;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author IFSC
- */
 public class GithubRobson {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+       /* Scanner leia = new Scanner(System.in);
+        String nome = leia.nextLine();
+        System.out.println(nome+"Bem-vindo ao GitHub");
+       */
+        String nome = JOptionPane.showInputDiaLog
+            (null, "Digite seu nome: ");
+        JOptionPane.showMenssageDialog
+            (null, nome+"Bem-vindo ao GitHub");
+        System.exit(0);
+       
+        
+        
+        
+        
     }
     
 }
